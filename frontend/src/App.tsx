c9217cfc5c4647560b78a2b3272fd540c2a8547c
Tyler Yee <yee.s.tyler@gmail.com>
2023-10-10 18:58:38 -0400

    Final commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,13 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from "./Home/Home";
 import Products from "./Products/Products";
+import Orders from './Orders/Orders';
+import OrderDetail from './Orders/OrderDetail';
+import CheckoutSteps from './Payment/CheckoutSteps';
+import PaymentScreen from './Payment/Payment';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from "./Home/Home";
+import Products from "./Products/Products";
 
 function App() {
     const openMenu = () => {
@@ -14,50 +21,105 @@ function App() {
         document.querySelector(".sidebar")?.classList.remove("open");
     }
 
-    return (
-        <Router>
-            <div className="grid-container">
-                <link rel="stylesheet" href="style.css" />
-                <title>Urban Memory</title>
-
-                <body>
-                    <div>
-                        <header className="header">
-                            <div className="brand">
-                                <button onClick={openMenu}>&#9776;</button>
-                                <a href="index.html">Urban Memory</a>
-                            </div>
-                            <div className="header-links">
-                                <Link to="/">Home</Link>
-                                <Link to="/catalog">Catalog</Link>
-                            </div>
-                        </header>
-                        <aside className="sidebar">
-                            <h3>Shopping Categories</h3>
-                            <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-                            <ul>
-                                <li>
-                                    <a href="index.html">Pants</a>
-                                </li>
-                                <li>
-                                    <a href="index.html">Shirts</a>
-                                </li>
-                            </ul>
-                        </aside>
-                        <main className="main">
-                            <Routes>
-                                <Route path="/" element={<Home />} />
-                                <Route path="/catalog" element={<Products />} />
-                            </Routes>
-                        </main>
-                        <footer>
-                            &copy; 2023 Urban Memory sdasds
-                        </footer>
-                    </div>
-                </body>
+  return (
+    <div className="grid-container">
+    <head>
+    <link rel="stylesheet" href="style.css" />
+    <title>Urban Memory</title>
+</head>
+<body>
+    <div>
+        <header className="header">
+            <div className="brand">
+                <button onClick={openMenu}>&#9776;</button>
+                <a href="index.html">Urban Memory</a>
             </div>
-        </Router>
-    );
+            <div className="header-links">
+                <a href="cart.html">Cart</a>
+                <a href="signin.html">Sign In</a>
+                </div>
+        </header>
+        <aside className = "sidebar">
+            <h3>Shopping Categories</h3>
+            <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+            <ul>
+                <li>
+                    <a href="index.html">Pants</a>
+                </li>
+                <li>
+                    <a href="index.html">Shirts</a>
+                </li>
+            </ul>
+        </aside>
+        <main>
+            <div className = "conent">
+                <ul className = "products">
+                    <li>
+                        <div className="product">
+                            <img className="product-image" src="images/d1.jpg" alt="product" />
+                            <div className = "product-name">
+                                <a href="product.html">Slim Shirt</a>
+                            </div>
+                            <div className="product-brand">Nike</div>
+                            <div className="product-price">$60</div>
+                            <div className="product-rating">4.5 Stars (10 reviews)</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="product">
+                            <img className="product-image" src="images/d1.jpg" alt="product" />
+                            <div className = "product-name">
+                                <a href="product.html">Slim Shirt</a>
+                            </div>
+                            <div className="product-brand">Nike</div>
+                            <div className="product-price">$60</div>
+                            <div className="product-rating">4.5 Stars (10 reviews)</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="product">
+                            <img className="product-image" src="images/d1.jpg" alt="product" />
+                            <div className = "product-name">
+                                <a href="product.html">Slim Shirt</a>
+                            </div>
+                            <div className="product-brand">Nike</div>
+                            <div className="product-price">$60</div>
+                            <div className="product-rating">4.5 Stars (10 reviews)</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="product">
+                            <img className="product-image" src="images/d1.jpg" alt="product" />
+                            <div className = "product-name">
+                                <a href="product.html">Slim Shirt</a>
+                            </div>
+                            <div className="product-brand">Nike</div>
+                            <div className="product-price">$60</div>
+                            <div className="product-rating">4.5 Stars (10 reviews)</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="product">
+                            <img className="product-image" src="images/d1.jpg" alt="product" />
+                            <div className = "product-name">
+                                <a href="product.html">Slim Shirt</a>
+                            </div>
+                            <div className="product-brand">Nike</div>
+                            <div className="product-price">$60</div>
+                            <div className="product-rating">4.5 Stars (10 reviews)</div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </main>
+        <footer>
+            &copy; 2023 Urban Memory sdasds
+        </footer>
+    </div>
+</body>
+</div>
+
+  );
 }
 
 export default App;
